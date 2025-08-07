16b36dca30ad9478c145ad9ce54ff9076cb8a12e
Rajat Joshi <41269874+rajatjoshii@users.noreply.github.com>
2025-08-07 23:25:28 +0530

    feat: replace header animation with local lottie

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -4,11 +4,12 @@ import { TypewriterService } from './typewriter.service';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatDividerModule,
+  imports: [MatButtonModule, MatIconModule, MatDividerModule, LottieComponent,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -16,6 +17,9 @@ import {MatButtonModule} from '@angular/material/button';
 export class HeaderComponent implements OnInit {
   type$!: Observable<string>;
   typewriter: string = "";
+  options: AnimationOptions = {
+    path: 'assets/lottie/header.json'
+  };
   @Output() navigateToBio = new EventEmitter<void>();
   @Output() navigateToExperience = new EventEmitter<void>();
   constructor(readonly typewriterService: TypewriterService){}
